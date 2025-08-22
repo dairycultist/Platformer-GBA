944a6bfd811ad11182ab5757558a8a00f48e1c19
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-21 20:38:15 -0400

    Figure out how tile and map data work

--- source/main.c
+++ source/main.c
@@ -5,7 +5,7 @@
 
 // display
 // relevant sections:
-// - LCD VRAM Overview
+// - VRAM https://www.problemkaputt.de/gbatek.htm#lcdvramoverview
 // - LCD I/O BG Control
 // - DISPCNT  https://www.problemkaputt.de/gbatek.htm#lcdiodisplaycontrol
 // - Palettes https://www.problemkaputt.de/gbatek.htm#lcdcolorpalettes
@@ -30,17 +30,19 @@ static inline uint16_t RGB15(uint16_t r, uint16_t g, uint16_t b) {
 
 int main(int argc, char *argv[]) {
 
-    memset((void *) &PALETTE, RGB15(31, 31, 31), 256);
-    
-    // set background mode to mode 0 and display BG 0 (not 123 for now)
-    DISPCNT = 0 | (0b0001 << 8);
+    // initialize rendering
+    memset((void *) &PALETTE, RGB15(31, 31, 31), 256);  // (temp) set palette to all white
+    PALETTE = RGB15(31, 10, 31);                        // set backdrop color to purple for testing
+    DISPCNT = 0 | (0b0001 << 8);                        // set background mode to mode 0 and display BG 0 (not 123 for now)
+    BG0CNT = (8 << 8);                                  // configure BG0CNT to take tile data from 0th sector, and map data from 8th sector
 
-    // configure BG0CNT
-    // BG0CNT = 
+    // write a test sprite to tile data
+    memset((void *) &VRAM, (uint16_t) (1 | (1 << 4)), 16);
 
-    while (1) {
+    // write a test map to map data
+    *(&VRAM + (0x400 * 8)) = 1;
 
-    }
+    while (1);
 
     return 0;
 }
